d67b2844a9fb812fd108f7cf4d51c53b112e10ad
Cam Smith <cam.smith@softwareofexcellence.com>
2020-12-09 16:50:31 +1300

    Tweak intro

--- rgb.py
+++ rgb.py
@@ -5,10 +5,10 @@
 #     - a method `to_hex_code()` that returns the hex representation
 #     - a method `to_rgb_dict()` that returns the r, g, b values as integers in a dictionary
 #
-# Checkout test_color_class.py to see what is expected. Tests are run with `pytest`
+# Checkout test_color_class.py to see what is expected. Tests are run with `pytest`. You can (and should!) run the tests
+# as often as you want.
 #
-# Feel free to look up *any* reference or information you need, ask any questions you want
-# and importantly, have fun! 
+# Feel free to look up *any* reference or information you need, ask any questions you want and importantly, have fun!
 
 
 def hex_to_rgb(hex_code):
