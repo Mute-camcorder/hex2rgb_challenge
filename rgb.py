06e5f9e07db4ccc80921547566cac31a8ec60fb9
Cam Smith <cam.smith@softwareofexcellence.com>
2020-12-09 16:48:07 +1300

    Improvements after first use in interview

--- rgb.py
+++ rgb.py
@@ -1,7 +1,9 @@
 # RGB.py
 # This file should contain:
 #   * a function that converts a hex code into a dictionary of r, g, b integers
-#   * a class `Colour` that can be instantiated by hex code or by a dictionary of r, g, b integers
+#   * a class `Colour` that can be instantiated by hex code or by a dictionary of r, g, b integers;
+#     - a method `to_hex_code()` that returns the hex representation
+#     - a method `to_rgb_dict()` that returns the r, g, b values as integers in a dictionary
 #
 # Checkout test_color_class.py to see what is expected. Tests are run with `pytest`
 #
--- test_color_class.py
+++ test_color_class.py
@@ -1,7 +1,10 @@
+
+# Start by importing our `rgb.py` file which we plan on testing
 import rgb
 
 SAMPLE_HAPPY_COLOURS = {
         "#000000": {"r": 0,   "g": 0,   "b": 0},
+        "#0FA5FF": {"r": 15,  "g": 165, "b": 255},
         "#172957": {"r": 23,  "g": 41,  "b": 87},
         "#800000": {"r": 128, "g": 0,   "b": 0},
         "#F42069": {"r": 244, "g": 32,  "b": 105},
@@ -9,24 +12,62 @@ SAMPLE_HAPPY_COLOURS = {
         "#ffFFff": {"r": 255, "g": 255, "b": 255},
     }
 
-def test_hex_to_rgb_function():
-    for k, v in SAMPLE_HAPPY_COLOURS.items():
-        assert v == rgb.hex_to_rgb(k)
+class TestPureFunctions:
+    def test_hex_to_rgb(self):
+        for k, v in SAMPLE_HAPPY_COLOURS.items():
+            assert v == rgb.hex_to_rgb(k)
 
+class TestColourClassSixHappy:
+    """
+    This class tests the `Colour` class, and only tests the 'happy-path'
+    """
 
-def test_create_by_hex():
-    for hex_code, rgb_dict in SAMPLE_HAPPY_COLOURS.items():
-        c = rgb.Colour(hex_code=hex_code)
-        assert c.r == rgb_dict["r"]
-        assert c.g == rgb_dict["g"]
-        assert c.b == rgb_dict["b"]
-        
-def test_create_by_dict():
-    for hex_code, rgb_dict in SAMPLE_HAPPY_COLOURS.items():
-        c = rgb.Colour(rgb_dict=rgb_dict)
-        assert c.to_hex_code().lower() == hex_code.lower()
+    def test_instantiate_by_hex(self):
+        """
+        Can we instantiate a Colour class with a 6 digit hex code as input? No Validation
+        """
+        for hex_code, rgb_dict in SAMPLE_HAPPY_COLOURS.items():
+            c = rgb.Colour(hex_code=hex_code)
+            assert type(c) == rgb.Colour
 
-        assert c.r == rgb_dict["r"]
-        assert c.g == rgb_dict["g"]
-        assert c.b == rgb_dict["b"]
-        
+    def test_instantiate_by_dict(self):
+        """
+        Can we instantiate a Colour class with a dictionary as input? No Validation
+        """
+        for hex_code, rgb_dict in SAMPLE_HAPPY_COLOURS.items():
+            c = rgb.Colour(rgb_dict=rgb_dict)
+            assert type(c) == rgb.Colour
+
+    def test_hex_code_object_to_hex_code(self):
+        """
+        Creates Colours using hex codes, then validates that the `to_hex_code` method returns the right value
+        """
+        for hex_code, rgb_dict in SAMPLE_HAPPY_COLOURS.items():
+            c = rgb.Colour(hex_code=hex_code)
+            # Note the test here calls `lower` because we don't have a perference for if the output is upper or lower
+            assert c.to_hex_code().lower() == hex_code.lower()
+
+    def test_rgb_dict_object_to_hex_code(self):
+        """
+        Creates Colours using rgb_dicts, then validates that the `to_hex_code` method returns the right value
+        """
+        for hex_code, rgb_dict in SAMPLE_HAPPY_COLOURS.items():
+            c = rgb.Colour(rgb_dict=rgb_dict)
+            # Note the test here calls `lower` because we don't have a perference for if the output is upper or lower
+            assert c.to_hex_code().lower() == hex_code.lower()
+
+    def test_hex_code_object_to_rgb_dict(self):
+        """
+        Creates colours using a dictionary, and calls `rgb_dict`, validating the output
+        """
+        for hex_code, rgb_dict in SAMPLE_HAPPY_COLOURS.items():
+            c = rgb.Colour(hex_code=hex_code)
+            assert c.to_rgb_dict() == rgb_dict
+
+    def test_rgb_dict_object_to_rgb_dict(self):
+        """
+        Creates colours using a dictionary, and calls `rgb_dict`, validating the output
+        """
+        for hex_code, rgb_dict in SAMPLE_HAPPY_COLOURS.items():
+            c = rgb.Colour(rgb_dict=rgb_dict)
+            assert c.to_rgb_dict() == rgb_dict
